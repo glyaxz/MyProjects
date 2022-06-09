d4b6387d4df16c67dac2bd24c494034fd162724e
glyaxz <javiky03@gmail.com>
2022-06-09 14:02:14 +0200

    Primer dia de curso - 22.Rutas con nombre

---
+++ example-app/example-app/resources/views/testing2.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php echo $msg ?> //Echo normal de php para printear una variable pasada por el Route::get
+    <?= $msg ?> //Echo abreviado
+    {{ $msg }} //Realizar un echo a una variable a traves del motor de templates de Blade/Laravel
+
+</body>
+</html>
--- example-app/example-app/routes/web.php
+++ example-app/example-app/routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testing', function(){
+    $msg = "[+] Mensaje desde el servidor";
+    return view('testing',['msg' => $msg]);
+    // se puede enviar variables a la view a traves de una array => [''='',*] para usarse en el view
+    // Para limpiar el código, se puede transferir la data a una variable externa dentro de Route::get()
+});
+
+Route::get('/testing2', function(){
+    $msg = "[+] Mensaje desde el servidor";
+    $data = ['msg' => $msg];
+
+    return view('testing', $data);
+});
+Route::get('/contacto',function(){
+    return view('contacto');
+});
+
+Route::get('/contactame', function(){
+    return view('contacto');
+})->name('contacto');
+// Para darle un nombre a la ruta vamos a usar ->name('[nombre]')
+// El name('[Nombre]') no tiene porque coincidir con el URI de Route::get('[URI]')
