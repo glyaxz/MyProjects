8a4310a8d10eb8e2b31d6dd5b922284b4396462c
glyaxz <javiky03@gmail.com>
2022-06-10 13:42:42 +0200

    Segundo dia de curso - 26.

--- example-app/example-app/routes/web.php
+++ example-app/example-app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+//use App\Http\Controllers\TestController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,7 +22,7 @@ Route::get('/', function () {
 Route::get('/testing', function(){
     $msg = "[+] Mensaje desde el servidor";
     return view('testing',['msg' => $msg]);
-    // se puede enviar variables a la view a traves de una array => [''='',*] para usarse en el view
+    // Se puede enviar variables a la view a traves de una array => [''='',*] para usarse en el view
     // Para limpiar el código, se puede transferir la data a una variable externa dentro de Route::get()
 });
 
@@ -37,5 +39,10 @@ Route::get('/contacto',function(){
 Route::get('/contactame', function(){
     return view('contacto');
 })->name('contacto');
-// Para darle un nombre a la ruta vamos a usar ->name('[nombre]')
-// El name('[Nombre]') no tiene porque coincidir con el URI de Route::get('[URI]')
+    // Para darle un nombre a la ruta vamos a usar ->name('[nombre]')
+    // El name('[Nombre]') no tiene porque coincidir con el URI de Route::get('[URI]')
+
+Route::get('/test',[App\Http\Controllers\TestController::class, 'test']);
+    //Existe 2 formas de identificar los controladores:
+        //1. En la cabecera del fichero con use
+        //2. En el Route directamente
